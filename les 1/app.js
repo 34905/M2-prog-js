550d3cc2de63682a4852bb4d7b5df22fdfc214fd
hasan <34905@ma-web.nl>
2022-11-24 10:52:47 +0100

    js en html

---
+++ les 1/app.js
@@ -0,0 +1,18 @@
+class App
+{
+    runApplication()
+    {
+        console.log("hello world");
+     let appNaam = "xzk";
+     let versienummer = 1.0;
+     let versiedatum = "11/24/2022";
+     let autheur = "hasan";
+     let copyright = "van hasan";
+     let distributeur = "hasan";
+     let darkmode = true
+       console.log (appNaam , versiedatum , versienummer , autheur , copyright , distributeur , darkmode)
+    }
+}
+
+let app = new App();
+app.runApplication();
